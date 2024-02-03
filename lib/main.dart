de835833f19da39c70612baa961f90aab1b034c0
Given <moretigiven@gmail.com>
2024-02-03 14:24:10 +0200

    Add images

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Snatcher",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              fontFamily: "RobotoMono",
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.orange[600],
-        ),
-        body: Center(
-          child: Text(
-            "Hello Given",
-            style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
-                fontFamily: 'RobotoMono'),
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Snatcher",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            fontFamily: "RobotoMono",
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("+"),
-          backgroundColor: Colors.orange[600],
+        centerTitle: true,
+        backgroundColor: Colors.orange[600],
+      ),
+      body: Center(
+        child: Image.asset(
+          'assets/images/snatch2.jpg',
         ),
       ),
-    ));
-
-  class TestWidget extends StatelessWidget {
-    const TestWidget({super.key});
-  
-    @override
-    Widget build(BuildContext context) {
-      return Container();
-    }
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Go"),
+        backgroundColor: Colors.orange[600],
+        elevation: 20,
+      ),
+    );
   }
+}
